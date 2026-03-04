ab668a589522591dfc3c0fb6ce4447e79c2857ce
KNKDarK <sshaik38@gitam.in>
2026-03-04 20:35:53 +0530

    Add Vercel Web Analytics integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/next"
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
@@ -47,6 +48,7 @@ export default function RootLayout({
         {children}
         <SpeedInsights />
         <Toaster />
+        <Analytics />
       </body>
     </html>
   );
